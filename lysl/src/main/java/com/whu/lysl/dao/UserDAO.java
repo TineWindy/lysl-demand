296d348899bfc5f9806537373f45c3856871c7f6
Norlan.xy <1670142089@qq.com>
2020-02-10 03:27:40 +0800

    配置一些文件

---
+++ lysl/src/main/java/com/whu/lysl/dao/UserDAO.java
@@ -0,0 +1,15 @@
+package com.whu.lysl.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * user dao
+ * @author Visionary
+ * @since 2020/2/10 3:00 AM
+ */
+@Mapper
+public interface UserDAO {
+
+    void insert();
+
+}
