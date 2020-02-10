3737ef9f17c18a0762152921eedab3f8071325d0
channel960608 <997476274@qq.com>
2020-02-10 10:22:45 +0800

    add 捐赠订单模块

---
+++ lysl/src/main/java/com/whu/lysl/dao/DonationOrderDAO.java
@@ -0,0 +1,31 @@
+package com.whu.lysl.dao;
+
+import com.whu.lysl.entity.condition.DonationOrderCondition;
+import com.whu.lysl.entity.condition.InstCondition;
+import com.whu.lysl.entity.dbobj.DonationOrderDO;
+import com.whu.lysl.entity.dto.DonationOrder;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+/**
+ * @Author Caspar
+ * @Date 2020/2/9 15:09
+ * @Description:
+ */
+@Mapper
+public interface DonationOrderDAO {
+
+    /**
+     * 根据条件查询机构
+     * @param donationOrderCondition 查询条件
+     * @return list
+     */
+    List<DonationOrderDO> selectByCondition(DonationOrderCondition donationOrderCondition);
+
+    int insertDonationOrder(DonationOrderDO donationOrder);
+
+    int updateDonationOrder(DonationOrderDO donationOrder);
+
+}
---
+++ lysl/src/main/java/com/whu/lysl/service/donation/impl/DonationOrderServiceImpl.java
@@ -0,0 +1,55 @@
+package com.whu.lysl.service.donation.impl;
+
+import com.whu.lysl.base.converters.DonationOrderConverter;
+import com.whu.lysl.dao.DonationOrderDAO;
+import com.whu.lysl.entity.condition.DonationOrderCondition;
+import com.whu.lysl.entity.dbobj.DonationOrderDO;
+import com.whu.lysl.entity.dto.DonationOrder;
+import com.whu.lysl.service.donation.DonationOrderService;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * @Author Caspar
+ * @CreateTime 2020/2/9 15:24
+ * @Description:
+ */
+@Service
+public class DonationOrderServiceImpl implements DonationOrderService {
+    @Resource
+    private DonationOrderDAO donationOrderDAO;
+
+    @Override
+    public List<DonationOrder> getDonationOrderByCondition(DonationOrderCondition donationOrderCondition) {
+        return DonationOrderConverter.batchDo2Model(donationOrderDAO.selectByCondition(donationOrderCondition));
+    }
+
+    @Override
+    public int insertDonationOrder(DonationOrder donationOrder) {
+        donationOrder.setStatus(0);
+        donationOrder.setDeleted(0);
+        return donationOrderDAO.insertDonationOrder(DonationOrderConverter.model2Do(donationOrder));
+    }
+
+    @Override
+    public int updateDonationOrder(DonationOrder donationOrder) {
+        return donationOrderDAO.updateDonationOrder(DonationOrderConverter.model2Do(donationOrder));
+    }
+
+    public int checkPass(DonationOrder donationOrder) {
+        DonationOrder donationOrder1 = new DonationOrder();
+        donationOrder1.setStatus(1);
+        donationOrder1.setDonationOrderId(donationOrder.getDonationOrderId());
+        return updateDonationOrder(donationOrder);
+    }
+
+    public int checkFail(DonationOrder donationOrder) {
+        DonationOrder donationOrder1 = new DonationOrder();
+        donationOrder1.setStatus(2);
+        donationOrder1.setDonationOrderId(donationOrder.getDonationOrderId());
+        return updateDonationOrder(donationOrder);
+    }
+
+}
