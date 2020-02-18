617948bfaf3dad48973a398e8c801df7469d19db
Norlan.xy <1670142089@qq.com>
2020-02-18 23:05:29 +0800

    添加通知服务

--- lysl/src/test/java/com/whu/lysl/LyslApplicationTests.java
+++ lysl/src/test/java/com/whu/lysl/LyslApplicationTests.java
@@ -1,5 +1,7 @@
 package com.whu.lysl;
 
+import com.whu.lysl.base.enums.LYSLMessageEnum;
+import com.whu.lysl.service.notice.NoticeService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mybatis.spring.annotation.MapperScan;
@@ -8,6 +10,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import javax.annotation.Resource;
 
 
 @SpringBootTest
@@ -17,8 +20,14 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @MapperScan("com.whu.lysl.dao")
 public class LyslApplicationTests {
 
+	@Resource
+	private NoticeService noticeService;
+
 	@Test
 	public void contextLoads() {
+
+//		noticeService.sendSingleMessage(LYSLMessageEnum.DONOR_SHIP, "18331131879", "jzh", "武汉大学", "口罩若干", "www.baidu.com", "15282329012");
+
 	}
 
 }
